2ba8ccbcdf6c9736e9992b7e33775c2a89e111c5
Arne Hitzmann <arne.hitzmann@gmail.com>
2017-08-26 04:06:49 +0000

    tested writing to GPIO0

--- tinkerboard_io.c
+++ tinkerboard_io.c
@@ -79,14 +79,14 @@ struct gpio_pin_t _gpio_pins[] = {
 	{.gpio_bank_offset = 0, .gpio_control_offset = 0, .grf_bank_offset = 0, .grf_pin_offset = 0, .is_gpio = 1, .mode = INPUT}
 };
 
-void _write_mem_b(volatile uint32_t* mem_addr, uint32_t value){
+void _write_mem(volatile uint32_t* mem_addr, uint32_t value){
 	//printf("Wrote %08X at %08X\n", value, (unsigned) mem_addr);
 	__sync_synchronize();
 	*mem_addr = value;
 	__sync_synchronize();
 }
 
-uint32_t _read_mem_b(volatile uint32_t* mem_addr){
+uint32_t _read_mem(volatile uint32_t* mem_addr){
 	uint32_t ret = 0;
 	__sync_synchronize();
 	ret = *mem_addr;
@@ -141,12 +141,12 @@ int tinkerboard_init(void){
 		printf("Error while mapping gpio block into virtual memory");
 	}
 
-	_rk3288_gpio_grf_base = _rk3288_gpio_block_base + RK3288_GPIO_GRF_OFFSET / 4;
-	_rk3288_gpio_0_base = _rk3288_gpio_block_base + RK3288_GPIO0_OFFSET / 4;
-	_rk3288_gpio_5_base = _rk3288_gpio_block_base + RK3288_GPIO5_OFFSET / 4;
-	_rk3288_gpio_6_base = _rk3288_gpio_block_base + RK3288_GPIO6_OFFSET / 4;
-	_rk3288_gpio_7_base = _rk3288_gpio_block_base + RK3288_GPIO7_OFFSET / 4;
-	_rk3288_gpio_8_base = _rk3288_gpio_block_base + RK3288_GPIO8_OFFSET / 4;
+	_rk3288_gpio_grf_base = _rk3288_gpio_block_base + ALIGN_TO_UINT32T(RK3288_GPIO_GRF_OFFSET);
+	_rk3288_gpio_0_base = _rk3288_gpio_block_base + ALIGN_TO_UINT32T(RK3288_GPIO0_OFFSET);
+	_rk3288_gpio_5_base = _rk3288_gpio_block_base + ALIGN_TO_UINT32T(RK3288_GPIO5_OFFSET);
+	_rk3288_gpio_6_base = _rk3288_gpio_block_base + ALIGN_TO_UINT32T(RK3288_GPIO6_OFFSET);
+	_rk3288_gpio_7_base = _rk3288_gpio_block_base + ALIGN_TO_UINT32T(RK3288_GPIO7_OFFSET);
+	_rk3288_gpio_8_base = _rk3288_gpio_block_base + ALIGN_TO_UINT32T(RK3288_GPIO8_OFFSET);
 
 	retcode = 1;
 
@@ -184,14 +184,17 @@ printf("%d", ALIGN_TO_UINT32T(16));*/
 	if(tinkerboard_init() == 1){
 		printf("Successfully initialized\n");
 
-		_write_mem_b(_rk3288_gpio_grf_base + 0x0054 / 4, (uint32_t) 0x02000000);
-		_read_mem_b(_rk3288_gpio_grf_base + 0x0054 / 4);
-		_write_mem_b(_rk3288_gpio_5_base + 0x0001, (uint32_t) 1<<19);
-		_read_mem_b(_rk3288_gpio_5_base + 0x0001);
+		_write_mem(_rk3288_gpio_grf_base + 0x0054 / 4, (uint32_t) 0x02000000);
+		_read_mem(_rk3288_gpio_grf_base + 0x0054 / 4);
+		_write_mem(_rk3288_gpio_5_base + 0x0001, (uint32_t) 1<<19);
+		_write_mem(_rk3288_gpio_0_base + 0x0001, (uint32_t) 1<<17);
+		_read_mem(_rk3288_gpio_5_base + 0x0001);
 		printf("GPIO init\n");
 
-		_write_mem_b(_rk3288_gpio_5_base, (uint32_t) 1<<19);
-		_write_mem_b(_rk3288_gpio_5_base, (uint32_t) 0);
+		_write_mem(_rk3288_gpio_0_base, (uint32_t) 1<<17);
+		_write_mem(_rk3288_gpio_5_base, (uint32_t) 1<<19);
+		_write_mem(_rk3288_gpio_0_base, (uint32_t) 0);
+		_write_mem(_rk3288_gpio_5_base, (uint32_t) 0);
 	}
 
 	tinkerboard_end();
